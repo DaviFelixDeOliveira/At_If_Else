79ed9ab662afdb480747a8d9c368b0db44913245
Davi Felix <davi.oliveira153@etec.sp.gov.br>
2024-04-30 14:58:30 -0300

    Atividade Utilizando a Função If e Else em PHP

--- temperatura/script.php
+++ temperatura/script.php
@@ -30,3 +30,70 @@ elseif ($temperatura > 25) {
 
 
 ?>'
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<button class="btn"> <a href="index.html">voltar    </a>
+</button>
+
+<style>
+    .btn {
+     position: absolute;
+     font-size: 17px;
+     text-transform: uppercase;
+     text-decoration: none;
+     padding: 1em 2.5em;
+     display: inline-block;
+     border-radius: 6em;
+     transition: all .2s;
+     border: none;
+     font-family: inherit;
+     font-weight: 500;
+     color: white;
+     background-color: red;
+     right: 900px;
+     top: 200px;
+     cursor: pointer;
+    }
+    
+    .btn:hover {
+     transform: translateY(-3px);
+     box-shadow: 0 10px 20px rgba(0, 0, 0, 0.2);
+    }
+    
+    .btn:active {
+     transform: translateY(-1px);
+     box-shadow: 0 5px 10px rgba(0, 0, 0, 0.2);
+    }
+    
+    .btn::after {
+        text-decoration: none;
+     content: "";
+     display: inline-block;
+     height: 100%;
+     width: 100%;
+     border-radius: 100px;
+     position: absolute;
+     top: 0;
+     left: 0;
+     z-index: -1;
+     transition: all .4s;
+    }
+    
+    .btn::after {
+     background-color: red;
+    }
+    
+    .btn:hover::after {
+     transform: scaleX(1.4) scaleY(1.6);
+     opacity: 0;
+    }
+</style>
+    
+</body>
+</html>
